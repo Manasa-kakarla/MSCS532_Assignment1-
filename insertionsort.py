558879f39ca2ad8bf73d84c223da849a97c8f678
Manasa Kakarla <vkakarla27517@ucumberlands.edu>
2024-09-01 15:35:25 -0500

    Insertion sort in python for decreasing order

--- insertionsort.py
+++ insertionsort.py
@@ -5,12 +5,12 @@ def insertion_sort_decreasing(arr):
         j = i-1
         # shift elements of arr[0 to i-1], that are greater than
         # temp, to one position ahead of their current position
-        while j >= 0 and arr[j] < temp:
+        while j >= 0 and temp > arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = temp
 
-a = [12, 11, 13, 5, 6]
+a = [7, 3, 83, 999, 500]
 print("original array:", a)
 insertion_sort_decreasing(a)
 print("sorted array in decreasing order:", a)
